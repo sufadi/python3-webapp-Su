bb44cdcedc55fa69ba6602283d3e0db60eb1c572
sufadi <332157261@qq.com>
2018-01-07 21:25:41 +0800

    day8 构建前端

--- www/app.py
+++ www/app.py
@@ -40,41 +40,40 @@ def init_jinja2(app, **kw):
             env.filters[name] = f
     app['__templating__'] = env
 
-async def logger_factory(app, handler):
-
-    async def logger(request):
+@asyncio.coroutine
+def logger_factory(app, handler):
+    @asyncio.coroutine
+    def logger(request):
         logging.info('Request: %s %s' % (request.method, request.path))
-        return (await handler(request))
+        # yield from asyncio.sleep(0.3)
+        return (yield from handler(request))
     return logger
 
-async def data_factory(app, handler):
-
-    async def parse_data(request):
+@asyncio.coroutine
+def data_factory(app, handler):
+    @asyncio.coroutine
+    def parse_data(request):
         if request.method == 'POST':
             if request.content_type.startswith('application/json'):
-                request.__data__ = await request.json()
+                request.__data__ = yield from request.json()
                 logging.info('request json: %s' % str(request.__data__))
             elif request.content_type.startswith('application/x-www-form-urlencoded'):
-                request.__data__ = await request.post()
+                request.__data__ = yield from request.post()
                 logging.info('request form: %s' % str(request.__data__))
-
-        return (await handler(request))
+        return (yield from handler(request))
     return parse_data
 
-async def response_factory(app, handler):
-
-    async def response(request):
-        logging.info('Response handler...request %s' % request)
-        r = await handler(request)
-
+@asyncio.coroutine
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
+        logging.info('Response handler...')
+        r = yield from handler(request)
         if isinstance(r, web.StreamResponse):
-            logging.info('web.StreamResponse % r' % r)
             return r
-
         if isinstance(r, bytes):
             resp = web.Response(body=r)
             resp.content_type = 'application/octet-stream'
-            logging.info('isinstance bytes % resp' % resp)
             return resp
 
         if isinstance(r, str):
@@ -90,21 +89,17 @@ async def response_factory(app, handler):
             logging.info('Response handler...__template__ %s' % r)
             template = r.get('__template__')
             if template is None:
-                resp = web.Response(body=json.dumps(
-                    r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset=utf-8'
                 logging.info('isinstance dict None % resp' % resp)
                 return resp
             else:
-                resp = web.Response(
-                    body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 logging.info('isinstance dict templating % resp' % resp)
                 return resp
-
-        if isinstance(r, int) and r >= 100 and r < 600:
-            logging.info('isinstance int % r' % web.Response(r))
-            return web.Response(r)
+        if isinstance(r, int) and t >= 100 and t < 600:
+            return web.Response(t)
         if isinstance(r, tuple) and len(r) == 2:
             t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
@@ -132,8 +127,9 @@ def datetime_filter(t):
     dt = datetime.fromtimestamp(t)
     return u'%s-%s-%s' % (dt.year, dt.month, dt.day)
 
-async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='', db='sufadi')
+@asyncio.coroutine
+def init(loop):
+    yield from orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='', db='sufadi')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory
     ])
@@ -144,7 +140,7 @@ async def init(loop):
     add_static(app)
     # 运行web服务,服务器启动后,有用户在浏览器访?,就可以做出对应的响应
     # 127.0.0.1 本机地址
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
--- www/handlers.py
+++ www/handlers.py
@@ -16,10 +16,17 @@ from model import User, Comment, Blog, next_id
 
 
 @get('/')
-async def index(request):
-    logging.info('handlers.py handlers index: %s' % request)
-    users = await User.findAll()
+def index(request):
+    summary = 'Python学习小组-王大锤，谌铁柱，凌二蛋，苏总'
+    blogs = [
+        Blog(id='1', name='王大锤和美美的爱情故事', summary=summary,
+             created_at=time.time() - 120),
+        Blog(id='2', name='凌二蛋到底什么时候结婚', summary=summary,
+             created_at=time.time() - 3600),
+        Blog(id='3', name='谌铁柱的期权变现走向人生巅峰', summary=summary,
+             created_at=time.time() - 7200)
+    ]
     return {
-        "__template__": "home.html",
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
